b914a9e11f6772e4aea918122466a33547d03edc
MaurilioSemeao <106936169+MaurilioSemeao@users.noreply.github.com>
2022-09-08 00:10:38 -0300

    corrige conflito

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,14 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+<<<<<<< Updated upstream
+=======
+app.get('/mars',function(req, res){
+  res.send('Olá Marte!\n');
+});
+
+
+>>>>>>> Stashed changes
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
